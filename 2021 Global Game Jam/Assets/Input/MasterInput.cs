80cfb4b52873ac6d393d6b8e18c7aa025384079f
JayMarBu <jaymarsden08@yahoo.co.uk>
2021-01-30 21:54:30 +0000

    Player controls fully gamepad compatible

--- 2021 Global Game Jam/Assets/Input/MasterInput.cs
+++ 2021 Global Game Jam/Assets/Input/MasterInput.cs
@@ -73,6 +73,14 @@ public class @MasterInput : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""CursorMove"",
+                    ""type"": ""Value"",
+                    ""id"": ""097a4402-1d71-4880-a93e-e87a760f88e9"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -210,17 +218,6 @@ public class @MasterInput : IInputActionCollection, IDisposable
                 },
                 {
                     ""name"": """",
-                    ""id"": ""3d60aa37-c4d3-495f-a631-2ac092197ef3"",
-                    ""path"": ""<Gamepad>/rightStick"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Gamepad"",
-                    ""action"": ""MousePos"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
                     ""id"": ""65a54c42-7475-474c-a620-4f61001e3817"",
                     ""path"": ""<Keyboard>/escape"",
                     ""interactions"": """",
@@ -304,6 +301,17 @@ public class @MasterInput : IInputActionCollection, IDisposable
                     ""processors"": """",
                     ""groups"": ""Gamepad"",
                     ""action"": ""Dialogue"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""3d60aa37-c4d3-495f-a631-2ac092197ef3"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""CursorMove"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -540,6 +548,7 @@ public class @MasterInput : IInputActionCollection, IDisposable
         m_PlayerControls_Pause = m_PlayerControls.FindAction("Pause", throwIfNotFound: true);
         m_PlayerControls_Claw = m_PlayerControls.FindAction("Claw", throwIfNotFound: true);
         m_PlayerControls_Dialogue = m_PlayerControls.FindAction("Dialogue", throwIfNotFound: true);
+        m_PlayerControls_CursorMove = m_PlayerControls.FindAction("CursorMove", throwIfNotFound: true);
         // MenuNavigation
         m_MenuNavigation = asset.FindActionMap("MenuNavigation", throwIfNotFound: true);
         m_MenuNavigation_Select = m_MenuNavigation.FindAction("Select", throwIfNotFound: true);
@@ -603,6 +612,7 @@ public class @MasterInput : IInputActionCollection, IDisposable
     private readonly InputAction m_PlayerControls_Pause;
     private readonly InputAction m_PlayerControls_Claw;
     private readonly InputAction m_PlayerControls_Dialogue;
+    private readonly InputAction m_PlayerControls_CursorMove;
     public struct PlayerControlsActions
     {
         private @MasterInput m_Wrapper;
@@ -614,6 +624,7 @@ public class @MasterInput : IInputActionCollection, IDisposable
         public InputAction @Pause => m_Wrapper.m_PlayerControls_Pause;
         public InputAction @Claw => m_Wrapper.m_PlayerControls_Claw;
         public InputAction @Dialogue => m_Wrapper.m_PlayerControls_Dialogue;
+        public InputAction @CursorMove => m_Wrapper.m_PlayerControls_CursorMove;
         public InputActionMap Get() { return m_Wrapper.m_PlayerControls; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -644,6 +655,9 @@ public class @MasterInput : IInputActionCollection, IDisposable
                 @Dialogue.started -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnDialogue;
                 @Dialogue.performed -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnDialogue;
                 @Dialogue.canceled -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnDialogue;
+                @CursorMove.started -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnCursorMove;
+                @CursorMove.performed -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnCursorMove;
+                @CursorMove.canceled -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnCursorMove;
             }
             m_Wrapper.m_PlayerControlsActionsCallbackInterface = instance;
             if (instance != null)
@@ -669,6 +683,9 @@ public class @MasterInput : IInputActionCollection, IDisposable
                 @Dialogue.started += instance.OnDialogue;
                 @Dialogue.performed += instance.OnDialogue;
                 @Dialogue.canceled += instance.OnDialogue;
+                @CursorMove.started += instance.OnCursorMove;
+                @CursorMove.performed += instance.OnCursorMove;
+                @CursorMove.canceled += instance.OnCursorMove;
             }
         }
     }
@@ -765,6 +782,7 @@ public class @MasterInput : IInputActionCollection, IDisposable
         void OnPause(InputAction.CallbackContext context);
         void OnClaw(InputAction.CallbackContext context);
         void OnDialogue(InputAction.CallbackContext context);
+        void OnCursorMove(InputAction.CallbackContext context);
     }
     public interface IMenuNavigationActions
     {
