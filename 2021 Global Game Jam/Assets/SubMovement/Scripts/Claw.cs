617dc96357500ff47a04b0ec48bc30d74ee3cdcb
sandycfisher <1801486@uad.ac.uk>
2021-01-30 04:22:45 +0000

    Started upgradables
    Created a player stats script and tried to fix grabber bug

--- 2021 Global Game Jam/Assets/SubMovement/Scripts/Claw.cs
+++ 2021 Global Game Jam/Assets/SubMovement/Scripts/Claw.cs
@@ -10,6 +10,7 @@ public class Claw : MonoBehaviour
     private Rigidbody joint_rb;
 
     bool triggeringWithUpgrade = false;
+    GameObject triggeringUpgrade;
 
     // Start is called before the first frame update
     void Start()
@@ -20,26 +21,34 @@ public class Claw : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         rb.position = new Vector2(joint_rb.position.x, joint_rb.position.y);
+
+        if(triggeringWithUpgrade == true && triggeringUpgrade)
+        {
+            Rigidbody2D upgrade_rb = triggeringUpgrade.GetComponent<Rigidbody2D>();
+            upgrade_rb.position = rb.position;
+        }
+
     }
-
-
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Upgrade")
-        {
+        Debug.Log("Colliding with treasure");
+        if(other.tag == "Treasure")
+        {           
+            triggeringUpgrade = other.gameObject;
             triggeringWithUpgrade = true;
-            Debug.Log("Colliding with Upgrade");
         }
     }
 
     public void OnTriggerExit2D(Collider2D other)
     {
-        if(other.tag == "Upgrade")
+        if(other.tag == "Treasure")
         {
+            Debug.Log("Leaving treasure");
+            triggeringUpgrade = null;
             triggeringWithUpgrade = false;
-            Debug.Log("Leaving Upgrade");
         }
     }
 
---
+++ 2021 Global Game Jam/Assets/SubMovement/Scripts/PlayerStats.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    public float torchRange;
+    public float torchDuration;
+    public float moveSpeed;
+    public float sonarRange;
+    public float sonarSpeed;
+    public float hullDurability;
+    public float hullHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
