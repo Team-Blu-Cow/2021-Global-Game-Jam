3791cfc11808abce7869d34e2ea231e8a6592918
Adam Emslie <44575716+yaboiadam00@users.noreply.github.com>
2021-01-30 19:11:36 +0000

    Pause Menu
     - pause menu fully implemented

--- 2021 Global Game Jam/Assets/UI/PauseMenu.cs
+++ 2021 Global Game Jam/Assets/UI/PauseMenu.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Canvas pauseMenu;
+    public MasterInput controls;
+    private bool paused = false;
+
+    private void Awake()
     {
-        
+        controls = new MasterInput();
+        controls.PlayerControls.Pause.performed += ctx => PauseGame();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    private void Start()
+    {
+        if (pauseMenu)
+        {
+            pauseMenu.enabled = paused;
+        }
+    }
+
+    public void PauseGame()
+    {
+        Debug.Log("Paused Pressed");
+        if (pauseMenu && paused)
+        {
+            Time.timeScale = 0f;
+            pauseMenu.enabled = paused;
+            paused = !paused;
+            Debug.Log("Game paused");
+        }
+        else if (pauseMenu && !paused)
+        {
+            Time.timeScale = 1f;
+            pauseMenu.enabled = paused;
+            paused = !paused;
+            Debug.Log("Game unpaused");
+        }
     }
 }
