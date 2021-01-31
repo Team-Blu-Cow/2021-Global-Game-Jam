700a32b4e517f782f48bd35b5826873536e5ba63
JayMarBu <jaymarsden08@yahoo.co.uk>
2021-01-31 04:24:49 +0000

    global light decreases upon descent

--- 2021 Global Game Jam/Assets/MiniMap/Scripts/MapManager.cs
+++ 2021 Global Game Jam/Assets/MiniMap/Scripts/MapManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 namespace minimap
 {
@@ -16,6 +17,8 @@ namespace minimap
         public RectTransform mapTransform;
         public RectTransform playerIconTransform;
         public SpriteRenderer spriteRenderer;
+        public Light2D globalLight;
+        
 
         void Awake()
         {
@@ -33,6 +36,8 @@ namespace minimap
             setMapSize();
             playerIconPos = TranslateWorldToMapPosition(playerTransform.position);
 
+            globalLight.intensity = NormaliseMapCoords(playerTransform.position).y*1.1f;
+
             playerIconTransform.anchoredPosition = new Vector2(playerIconPos.x, playerIconPos.y);
             
         }
@@ -40,12 +45,8 @@ namespace minimap
         public Vector3 TranslateWorldToMapPosition(Vector3 position)
         {
             // STEP1: normalize world coords
-            Vector3 max_w_pos = new Vector3(mapWidth / 2.0f, mapHeight / 2.0f, 0);
-            Vector3 min_w_pos = new Vector3(-(mapWidth / 2.0f), -(mapHeight / 2.0f), 0);
             Vector3 n_pos = new Vector3();
-
-            n_pos.x = Mathf.InverseLerp((float)min_w_pos.x, (float)max_w_pos.x, (float)position.x);
-            n_pos.y = Mathf.InverseLerp((float)min_w_pos.y, (float)max_w_pos.y, (float)position.y);
+            n_pos = NormaliseMapCoords(position);
 
             // STEP2: convert normalized coords to local map space
             Vector3 max_lm_pos = new Vector3( mapTransform.rect.width / 2, mapTransform.rect.width / 2, 0);
@@ -56,6 +57,18 @@ namespace minimap
             lm_pos.y = Mathf.Lerp(min_lm_pos.y, max_lm_pos.y, n_pos.y);
 
             return lm_pos;
+        }
+
+        private Vector3 NormaliseMapCoords(Vector3 in_pos)
+        {
+            Vector3 max_w_pos = new Vector3(mapWidth / 2.0f, mapHeight / 2.0f, 0);
+            Vector3 min_w_pos = new Vector3(-(mapWidth / 2.0f), -(mapHeight / 2.0f), 0);
+            Vector3 n_pos = new Vector3();
+
+            n_pos.x = Mathf.InverseLerp((float)min_w_pos.x, (float)max_w_pos.x, (float)in_pos.x);
+            n_pos.y = Mathf.InverseLerp((float)min_w_pos.y, (float)max_w_pos.y, (float)in_pos.y);
+
+            return n_pos;
         }
 
 
