855805c9522f63122ab3ea2b0850442d4da8ec4a
jack leslie <57846971+Flapjack432@users.noreply.github.com>
2021-01-31 04:33:38 +0000

    Added "LeanTween" for ui anim
    Added Lean tween for the ui animations and added the animations for the ui

--- 2021 Global Game Jam/Assets/MiniMap/Scripts/DisplayDialog.cs
+++ 2021 Global Game Jam/Assets/MiniMap/Scripts/DisplayDialog.cs
@@ -5,18 +5,69 @@ using UnityEngine;
 
 public class DisplayDialog : MonoBehaviour
 {
+    public MasterInput controls;
+
     TextMeshProUGUI tmp;
+    Queue<string> sentances;
 
     // Start is called before the first frame update
     void Start()
     {
-        tmp = GetComponent<TextMeshProUGUI>();
+        sentances = new Queue<string>();
+        tmp = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void Awake()
+    {
+        controls = new MasterInput();
+        controls.PlayerControls.Dialogue.performed += ctx => DisplayNextSentace();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    public void StartDialog(string[] customDialog)
+    {
+        sentances.Clear();
+
+        LeanTween.move(gameObject, new Vector3(transform.position.x, -20,0), 1);
+
+        foreach(string sentance in customDialog)
+        {
+            sentances.Enqueue(sentance);
+        }
+
+        DisplayNextSentace();
+    }
+
+    public void DisplayNextSentace()
+    {
+        if (sentances.Count == 0)
+        {
+            EndDialog();
+        }
+        else
+        {
+            StopAllCoroutines();
+            StartCoroutine(TypeSentance(sentances.Dequeue(), 0.1f));
+        }
+    }
+
+    void EndDialog()
+    {
+        LeanTween.move(gameObject, new Vector3(transform.position.x, -490, 0), 1);
     }
 
     public IEnumerator TypeSentance(string sentance, float delay)
@@ -26,8 +77,6 @@ public class DisplayDialog : MonoBehaviour
         {
             tmp.text += letter;
             yield return new WaitForSeconds(delay);
-        }
-        yield return new WaitForSeconds(10);
-        tmp.text = "";
+        }     
     }
 }
